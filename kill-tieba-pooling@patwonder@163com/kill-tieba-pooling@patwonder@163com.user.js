bd5484fdec54e7449ff1225bd1ccaa279275ae8d
Yifan Wu <patwonder@163.com>
2017-03-25 21:06:57 +0800

    KTP & TRAP: 兼容贴吧 https

--- kill-tieba-pooling@patwonder@163com/kill-tieba-pooling@patwonder@163com.user.js
+++ kill-tieba-pooling@patwonder@163com/kill-tieba-pooling@patwonder@163com.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @id             kill-tieba-pooling@patwonder@163.com
 // @name           贴吧内存泄漏修复 Tieba Memory Leak Fix
-// @version        1.1
+// @version        1.2
 // @namespace      patwonder@163.com
 // @author         patwonder
 // @description    
 // @updateURL      https://raw.githubusercontent.com/patwonder/UserScripts/master/kill-tieba-pooling@patwonder@163com/kill-tieba-pooling@patwonder@163com.user.js
 // @downloadURL    https://raw.githubusercontent.com/patwonder/UserScripts/master/kill-tieba-pooling@patwonder@163com/kill-tieba-pooling@patwonder@163com.user.js
-// @include        http://tieba.baidu.com/*
+// @include        /^https?://tieba\.baidu\.com/.*/
 // @run-at         document-end
 // ==/UserScript==
 
@@ -15,7 +15,7 @@
   var fails = 0;
   var poolcommon = function() {
     try {
-      var embed = d.querySelector("embed[src=\"http://play.baidu.com/player/static/flash/fmp.swf\"]");
+      var embed = d.querySelector("embed[src*=\"//play.baidu.com/player/static/flash/fmp.swf\"]");
       embed = (embed.wrappedJSObject || embed);
       embed.PercentLoaded = function() { return 100; };
       embed.setData = function() {};
--- tieba-remove-ajax-pager@patwonder@163com/tieba-remove-ajax-pager@patwonder@163com.user.js
+++ tieba-remove-ajax-pager@patwonder@163com/tieba-remove-ajax-pager@patwonder@163com.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @id             tieba-remove-ajax-pager@patwonder@163.com
 // @name           贴吧去除ajax翻页
-// @version        1.3
+// @version        1.4
 // @namespace      patwonder@163.com
 // @author         patwonder
 // @description    
 // @updateURL      https://raw.githubusercontent.com/patwonder/UserScripts/master/tieba-remove-ajax-pager@patwonder@163com/tieba-remove-ajax-pager@patwonder@163com.user.js
 // @downloadURL    https://raw.githubusercontent.com/patwonder/UserScripts/master/tieba-remove-ajax-pager@patwonder@163com/tieba-remove-ajax-pager@patwonder@163com.user.js
+// @include        /^https?://tieba\.baidu\.com/((f\?kz=.*)|(p/.*))/
 // @include        http://tieba.baidu.com/club/*/p/*
 // @include        http://tieba.baidu.com/f?kz=*
 // @include        http://tieba.baidu.com/p/*
